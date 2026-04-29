cd8a1e44bd50d18b53a9540f4f73a8230aaeb5fa
ahmedsamir122 <eng.ahmed.elmitwalli@gmail.com>
2026-04-29 21:57:37 +0300

    edit cors 5

--- api/app.js
+++ api/app.js
@@ -20,17 +20,18 @@ const app = express();
 
 app.use(helmet());
 
-app.use(
-  cors({
-    credentials: true,
-    origin: ["http://localhost:5173", "https://car-system-fawn.vercel.app"],
-  })
-);
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: ["http://localhost:5173", "https://car-system-fawn.vercel.app"],
+//   })
+// );
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://car-system-fawn.vercel.app"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.options("*", cors());
 
 const limiter = rateLimit({
